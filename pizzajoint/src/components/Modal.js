187b668bacb404f84b9946a45a6a9c83ff502617
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-25 02:07:34 +0530

    Modal, onExitComplete

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -4,10 +4,36 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 const backdropVariants = {
     visible: { opacity: 1 },
-    hidden: { opacity: 0}
+    hidden: { opacity: 0},
+    exit: { opacity: 0}
+}
+
+const modalVariants = {
+    hidden: {
+        y : '-100vh',
+        opacity : 0,
+        scale: 0.2
+    },
+    visible: {
+        y: 200,
+        opacity: 1,
+        scale: 1,
+        transition: { delay: 0.5 }
+    },
+    exit: {
+        y: '100vh',
+        opacity: 0,
+        scale: 0.2,
+        transition: {
+            ease: "easeOut",
+        }
+    }
 }
 
 const Modal = ({ showModal, setShowModal }) => {
+    const handleClick = () => {
+        setShowModal(false); // Doesn't work if we go back!!!
+    }
     return (  
         <AnimatePresence exitBeforeEnter>
             { showModal &&
@@ -16,8 +42,17 @@ const Modal = ({ showModal, setShowModal }) => {
                     variants= { backdropVariants }
                     animate = "visible"
                     initial = "hidden"
+                    exit = "exit"
                 >
-
+                    <motion.div 
+                        className = "modal"
+                        variants = { modalVariants }
+                    >
+                        <p>Want to make another pizza?</p>
+                        <Link to="/">
+                            <button onClick = { handleClick }>Start Again</button>
+                        </Link>
+                    </motion.div>
                 </motion.div>
             }
         </AnimatePresence>
