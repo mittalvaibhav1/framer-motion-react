4804c953e93dee3386c91a8512a8aac931bb5f37
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-24 19:05:00 +0530

    Keyframes

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion }  from 'framer-motion';
 
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1], // Keyframes
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)"
+  }
+}
+
 const Home = () => {
   return (
     <motion.div className="home container"
@@ -14,11 +22,9 @@ const Home = () => {
       </h2>
       <Link to="/base">
         <motion.button 
-          whileHover={{ 
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)"
-          }}
+          animate = "visible"
+          variants = { buttonVariants }
+          whileHover = "hover"
         >
           Create Your Pizza
         </motion.button>
--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -13,9 +13,20 @@ const containerVariants = {
     scale: 1,
     transition:  {
       type: 'spring',
-      delay: 0.5,
-      stiffness: 90
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4
     }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
   }
 }
 
@@ -28,8 +39,10 @@ const Order = ({ pizza }) => {
       animate = "visible"
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      <motion.p variants = {childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants = {childVariants}>
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
     </motion.div>
   )
 }
